749c00c3de194733dce7c0b9c8cf8a7501f423ef
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-09-18 19:20:26 -0700

    models.py

--- server/models.py
+++ server/models.py
@@ -7,32 +7,58 @@ class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String)
-    password_hash = db.Column(db.String)
-    email = db.Column(db.String)
-    address = db.Column(db.String)
-    phone_number = db.Column(db.String)
-    payment_card = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    username = db.Column(db.String(25), unique = True, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique = True, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    phone_number = db.Column(db.String(10), unique = True, nullable=False)
+    payment_card = db.Column(db.String(16), nullable=False)
 
 class Product(db.Model, SerializerMixin):
-    __tablename__ = 'recipes'
+    __tablename__ = 'products'
     
     id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String)
-    price = db.Column(db.Integer)
-    description = db.Column(db.String)
-    category = db.Column(db.String)
-    image = db.Column(db.String)
-    rating = db.Column(db.String)
+    title = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+    rating = db.Column(db.String, nullable=False)
 
 class Cart(db.Model, SerializerMixin):
-    __tablename__ = 'recipes'
+    __tablename__ = 'cart'
     
     id = db.Column(db.Integer, primary_key = True)
-    pass
+    product_id = db.Column(db.String, nullable=False)
+    product_name = db.Column(db.String, nullable=False)
+    product_quantity = db.Column(db.Integer, nullable=False)
+    product_price = db.Column(db.Float, nullable=False)
+    product_image = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.String, nullable=False)
 
 class Order(db.Model, SerializerMixin):
-    __tablename__ = 'recipes'
+    __tablename__ = 'orders'
     
     id = db.Column(db.Integer, primary_key = True)
-    pass
+    product_name = db.Column(db.String, nullable=False)
+    product_price = db.Column(db.Integer, nullable=False)
+    product_quantity = db.Column(db.Integer, nullable=False)
+    order_date = db.Column(db.String, nullable=False)
+    order_status = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.String, db.ForeinKey('users.id'))
+
+        
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    product_id = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.String, nullable=False)
+    body = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    user_id = db.Column(db.String, nullable=False)
+
+    product = db.relationship("Product", back_populates = "reviews")
+    user = db.relationship("User", back_populates = "reviews")
